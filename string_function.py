dac673cf4c3a0f5ac305e372968bcf78bdbac2d7
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-26 21:08:47 +0600

    string functions

---
+++ string_function.py
@@ -0,0 +1,14 @@
+story = "once upon a time there was a king named Alber Hook who is really bad"
+
+#String Function
+print(len(story))
+print(story.endswith("bad"))
+print(story.count("i"))
+print(story.capitalize())
+print(story.find("a"))
+print(story.replace("Alber ","James"))
+
+#Escape sequance character
+
+same = "He is a king.\nHe\tis ve\\ry good"
+print(same)
