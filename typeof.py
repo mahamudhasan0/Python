9d12658b045dce8c5bb675695c6a72bb48b5b02a
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-26 14:04:42 +0600

    types of variable

---
+++ typeof.py
@@ -0,0 +1,30 @@
+a="ammy"
+b='''summy'''
+d='lummy'
+e=345
+c=45.32
+f=True
+g=None
+
+#printing the variable
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+#printing  the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
+
+
+
+
