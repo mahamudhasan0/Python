9cbda7354cc219aa8ebfc1dc056789eff97ca943
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-26 20:09:32 +0600

    practice

---
+++ practice_set.py
@@ -0,0 +1,22 @@
+#add two no
+a = 12
+b = 30
+print("The sum of a and b is", a+b)
+
+#remainder
+a = 7
+b = 8
+print("The remainder when a is devided by b is", a%b)
+
+#input function 
+a = input("Enter a number: ")
+print(type(a))
+
+#average
+a = input("Enter first number")
+b = input("Enter the second number")
+a = int(a)
+b = int(b)
+avg = (a+b)/2
+print("The average value of a and b is ", avg)
+
