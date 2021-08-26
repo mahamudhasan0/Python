7e81097951b72e70c83dde3563cd1f90944144b8
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-26 14:36:36 +0600

    arithmetic operation

---
+++ expression.py
@@ -0,0 +1,31 @@
+a = 3
+b = 4
+
+#Arithmetic Operators
+print("The value of 3+4 is",3+4)
+print("The value of 3-4 is",3-4)
+print("The value of 3+4 is",3*4)
+print("The value of 3+4 is",3/4)
+
+#Assignment Operators
+a = 30
+a += 4
+a -= 4
+a *= 4
+a /= 4
+print(a)
+
+# Comparison Operators
+b =(14<8)
+# b =(14<=8)
+# b =(14==8)
+# b =(14>8)
+# b =(14!=8)
+print(b)
+
+#Logical Operators
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is",(bool1 and bool2))
+print("The value of bool1 or bool2 is",(bool1 or bool2))
+print("The value of not bool2 is",(not bool2))
