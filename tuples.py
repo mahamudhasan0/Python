857cae577e1e1335d81422f91df122f6254c2547
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-27 12:27:28 +0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# Creating a tuple using ()
+t = (1,2,3,4)
+
+t1 = () # Empty tuple
+t1 = (1) # Wrong way to declare a Tuple with single element
+t1 = (1,) # Tuple with single element
+
+# printing the element of the tuple
+print(t[0])
+
+# Cannot update the value of a tuple
+
+# Tuple method
+a = (11,23,23,11,20,29)
+print(a.count(11))
+print(a.index(23))
