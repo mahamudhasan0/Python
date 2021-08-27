bafc58ddebbd0aae4be91424e87525e9dca5f7ab
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-27 12:52:35 +0600

    problem set 3

---
+++ problem_set03.py
@@ -0,0 +1,34 @@
+# Store Fruits
+fruit1 = input("Enter Fruit No 1: ")
+fruit2 = input("Enter Fruit No 2: ")
+fruit3 = input("Enter Fruit No 3: ")
+fruit4 = input("Enter Fruit No 4: ")
+fruit5 = input("Enter Fruit No 5: ")
+fruit6 = input("Enter Fruit No 6: ")
+fruit7 = input("Enter Fruit No 7: ")
+
+myFruitList = [fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7]
+print(myFruitList) 
+
+# Store marks
+m1 = int(input("Enter marks for student No 1: "))
+m2 = int(input("Enter marks for student No 2: "))
+m3 = int(input("Enter marks for student No 3: "))
+m4 = int(input("Enter marks for student No 4: "))
+m5 = int(input("Enter marks for student No 5: "))
+m6 = int(input("Enter marks for student No 6: "))
+
+myListMarks = [m1, m2, m3, m4, m5, m6]
+myListMarks.sort()
+print(myListMarks)
+
+# sum of 4 no into the list
+a = [3, 5, 8, 9]
+print(a[0] + a[1] + a[2] + a[3])
+print(sum(a))
+
+# Count the number of tuple
+a = (7, 0, 8, 0, 9)
+print(a.count(0))
+
+
