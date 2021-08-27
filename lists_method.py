7340b51cc582d1484d899d8c587cae7b617273f8
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-27 12:15:31 +0600

    lists method

---
+++ lists_method.py
@@ -0,0 +1,15 @@
+# List Slicing
+friends = ["David","Jerry","Tom","Alice",40]
+print(friends[0:4])
+print(friends[-3:])
+
+#List Method
+list = [1,7,4,30,23,40]
+print(list)
+list.sort() #sorts the list
+list.reverse() #reverses list
+list.append(98) # adds 98 at the end of the list
+list.insert(2,67) # insert 67 index no 2
+list.pop(2) #remove element from index no 2
+list.remove(23) #remove 23 from the list
+print(list)
