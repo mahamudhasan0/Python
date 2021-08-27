4050cfc1f1b9561aeb682fd3e4c7e91483e2a1ab
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-27 11:11:51 +0600

    lists

---
+++ lists.py
@@ -0,0 +1,17 @@
+# create a list using []
+a = [1,5,34,9,4]
+
+# Print the list using print() function
+print(a)
+
+# Access using index a[0],a[1],a[2]
+print(a[4])
+
+# Change the value of list using
+a[1] = 40
+print(a)
+
+# We can create a list with items of different types
+b = [12,"Sam",False,6.6]
+print(b)
+
