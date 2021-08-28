a891da72c400969d963a1d3cf361f249ad3f9b81
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-08-28 11:07:59 +0600

    dictionary syntax

---
+++ dicrionary_syntex.py
@@ -0,0 +1,10 @@
+myDict = {
+    "Fast" : "In a Quick Manner",
+    "Mahamud" : "A coder",
+    "Marks" : [1,2,5],
+    "anotherDict" : {'Mahamud' : 'Player'}
+}
+print(myDict["Fast"])
+myDict["Marks"] = [9,10]
+print(myDict["Marks"])
+print(myDict["anotherDict"]["Mahamud"])
